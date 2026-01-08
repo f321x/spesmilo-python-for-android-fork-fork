e9fde5ab48edbc85a5ac9fb9de6347b76cd5b208
Sander van Grieken <sander@outrightsolutions.nl>
2026-01-08 21:51:35 +0100

    qt6: pyqt6/pyqt_builder/sip build

--- pythonforandroid/recipes/pyqt_builder/__init__.py
+++ pythonforandroid/recipes/pyqt_builder/__init__.py
@@ -1,17 +1,38 @@
-from pythonforandroid.recipe import PythonRecipe
+import toml
+from os.path import join
+from pythonforandroid.recipe import PythonRecipe, PyProjectRecipe
+from pythonforandroid.logger import info
 
-
-class PyQtBuilderRecipe(PythonRecipe):
+class PyQtBuilderRecipe(PyProjectRecipe):
     version = '1.19.1'
     url = "https://pypi.python.org/packages/source/P/PyQt-builder/pyqt_builder-{version}.tar.gz"
     name = 'pyqt_builder'
 
-    depends = ['sip', 'packaging']
+    depends = ['packaging']
+    hostpython_prerequisites = ['sip']
 
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
     install_in_targetpython = False  # FIXME broken
     site_packages_name = 'pyqtbuild'
 
+    def build_arch(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        with open(join(build_dir, 'pyproject.toml'), 'r') as f:
+            project_dict = toml.load(f)
+        info(repr(project_dict))
+        if 'license' in project_dict['project']:
+            del project_dict['project']['license']
+        if 'license-files' in project_dict['project']:
+            del project_dict['project']['license-files']
+        project_dict['tool']['setuptools'] = {'packages': [
+            'pyqtbuild', 'pyqtbuild.bundle', 'pyqtbuild.bundle.dlls',
+            'pyqtbuild.bundle.packages', 'pyqtbuild.bundle.qt_wheel_distinfo'
+        ]}
+
+        with open(join(build_dir, 'pyproject.toml'), 'w') as f:
+            toml.dump(project_dict, f)
+
+        super().build_arch(arch)
 
 recipe = PyQtBuilderRecipe()
--- pythonforandroid/recipes/sip/__init__.py
+++ pythonforandroid/recipes/sip/__init__.py
@@ -1,7 +1,9 @@
-from pythonforandroid.recipe import PythonRecipe
+import toml
+from os.path import join
+from pythonforandroid.recipe import PythonRecipe, PyProjectRecipe
+from pythonforandroid.logger import info
 
-
-class SipRecipe(PythonRecipe):
+class SipRecipe(PyProjectRecipe):
     version = '6.15.1'
     url = "https://pypi.python.org/packages/source/s/sip/sip-{version}.tar.gz"
     name = 'sip'
@@ -13,5 +15,40 @@ class SipRecipe(PythonRecipe):
     install_in_targetpython = False  # FIXME broken
     site_packages_name = 'sipbuild'
 
+    def build_arch(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        with open(join(build_dir, 'pyproject.toml'), 'r') as f:
+            project_dict = toml.load(f)
+        info(repr(project_dict))
+        if 'license' in project_dict['project']:
+            del project_dict['project']['license']
+        if 'license-files' in project_dict['project']:
+            del project_dict['project']['license-files']
+        project_dict['tool']['setuptools'] = {'packages': ['sipbuild']}
+        with open(join(build_dir, 'pyproject.toml'), 'w') as f:
+            toml.dump(project_dict, f)
+
+        # with current_directory(build_dir):
+        #     info("compiling sip")
+        #
+        #     hostpython = self.get_recipe('hostpython3', self.ctx)
+        #     pythondir = hostpython.get_path_to_python()
+        #     site_packages = join(pythondir, 'Lib', 'site-packages')
+        #     env = copy.copy(env)
+        #     env['PYTHONPATH'] = f'{site_packages}:' + env.get('PYTHONPATH', '')
+        #
+        #     buildcmd = sh.Command(self.ctx.hostpython)
+        #     # buildcmd = buildcmd.bake('-m', 'sipbuild.tools.install')
+        #     sip_install = join(pythondir, 'usr', 'local', 'bin', 'sip-install')
+        #     buildcmd = buildcmd.bake(sip_install)
+        #     buildcmd = buildcmd.bake('--confirm-license', '--qt-shared', '--verbose')
+        #     buildcmd = buildcmd.bake('--no-tools', '--no-qml-plugin', '--no-designer-plugin', '--no-dbus-python')
+        #
+        #     for include in self.BINDINGS:
+        #         buildcmd = buildcmd.bake('--enable', include)
+        #
+        #     shprint(buildcmd, _env=env, _tail=50, _critical=True)
+
+        super().build_arch(arch)
 
 recipe = SipRecipe()
