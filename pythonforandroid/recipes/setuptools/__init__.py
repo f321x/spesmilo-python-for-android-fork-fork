8f7a20a967ee73a06bbacdf70694e82fd9e0f1cd
Sander van Grieken <sander@outrightsolutions.nl>
2026-02-13 14:15:14 +0100

    setuptools 80.9.0

--- pythonforandroid/recipes/setuptools/__init__.py
+++ pythonforandroid/recipes/setuptools/__init__.py
@@ -3,6 +3,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class SetuptoolsRecipe(PyProjectRecipe):
     hostpython_prerequisites = ['setuptools']
+    version = '80.9.0'
 
 
 recipe = SetuptoolsRecipe()
